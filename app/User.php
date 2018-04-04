48697648ddb62d948318fecc737bc527dccccc1e
jamesx0517 <jamesx0517@gmail.com>
2018-04-05 07:57:42 +0800

    資料庫修改
    資料庫修改能儲存的欄位

--- app/User.php
+++ app/User.php
@@ -8,14 +8,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    protected $table ='users';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','pid',
     ];
 
     /**
@@ -29,7 +29,7 @@ class User extends Authenticatable
 
     public function id(){
 
-      return $this->hasone('App\Pcrepair','uid','id');
+      return $this->hasone('App\Pcrepair','it','id');
 
 
     }
