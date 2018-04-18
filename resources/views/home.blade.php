b4ac1911ad90702b16f59c77987c614b805a126c
jamesx0517 <jamesx0517@gmail.com>
2018-04-18 09:41:40 +0800

    新增聯結
    新增聯結

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
-
+@section('script')
+<script>
+var pid = {{Auth::user()->pid}}
+var id = {{Auth::user()->id}}
+if (pid==1){
+  $('#form').attr('href', '/pcrepairs/');
+}
+else{
+  $('#form').attr('href', '/clients/');
+}
+  </script>
+@endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -13,8 +24,9 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <a class="btn btn-primary" id=app  href='/application'>需求申請</a>
+                    <a class="btn btn-primary" id=form  href=>維修紀錄</a>
 
-                    You are logged in!
                 </div>
             </div>
         </div>
